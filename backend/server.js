921daceb5f4e12a35708f2546d23a21dce4a91fc
Destin Saba <destinsaba9@gmail.com>
2024-09-10 13:22:58 -0600

    Add mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 // Create express app
 const app = express();
 
 // middleware
+app.use(express.json());
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
@@ -15,8 +17,16 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts',workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT);
-})
+// connect to mongodb
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+    console.log('connected to db & listening on port', process.env.PORT);
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
 
